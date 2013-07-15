b64f19993cb48fbeb23ac5ff61c62d600c40b567
James <beardyjames@live.co.uk>
2013-07-15 12:13:42 +0100

    Add entity saving

--- src/com/tehbeard/forge/schematic/Blueprint.java
+++ src/com/tehbeard/forge/schematic/Blueprint.java
@@ -1,34 +1,93 @@
 package com.tehbeard.forge.schematic;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagDouble;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
 /**
  * Generates a schematic of an area of a world.
+ * Default behaviour mimics WorldEdit (save TE's, do not save entities)
  * @author James
  *
  */
 public class Blueprint {
 
-    private boolean noTileEntityData;
+    private boolean ignoreTileEntityData = false;//By default save tile entities
+    private boolean ignoreEntityData     = true;//By default 
+    
     private World world;
 
     private SchVector min;
     private SchVector max;
-    public Blueprint(boolean noTileEntityData, World world, SchVector v1,
+
+    
+    public Blueprint(World world, SchVector v1,
             SchVector v2) {
-        super();
-        this.noTileEntityData = noTileEntityData;
         this.world = world;
         this.min = SchVector.min(v1, v2);
         this.max = SchVector.max(v1, v2);
     }
+    
+    /**
+     * Set true to not capture TileEntity states
+     * @param ignoreTileEntityData
+     * @return
+     */
+    public Blueprint setIgnoreTileEntityData(boolean ignoreTileEntityData){
+        this.ignoreTileEntityData = ignoreTileEntityData;
+        return this;
+    }
+    
+    /**
+     * Set true to not capture entities
+     * @param ignoreEntityData
+     * @return
+     */
+    public Blueprint setIgnoreEntityData(boolean ignoreEntityData){
+        this.ignoreEntityData = ignoreEntityData;
+        return this;
+    }
+   
+    
 
     public SchematicFile createSchematicFile(){
         SchVector size = new SchVector(max).add(new SchVector(1,1,1));
         size.sub(min);
+        
         SchematicFile file = new SchematicFile((short)size.getX(), (short)size.getY(), (short)size.getZ());
+        
+        if(!ignoreEntityData){
+            AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(min.getX(), min.getY(), min.getZ(), max.getX(), max.getY(), max.getZ());
+            @SuppressWarnings("rawtypes")
+            List entities = world.getEntitiesWithinAABB(EntityCreature.class, bb);
+            
+            for(Object o : entities){
+                Entity e = (Entity)o;
+                if(e instanceof EntityPlayer){continue;}
+                NBTTagCompound tag = new NBTTagCompound();
+                e.writeToNBT(tag);
+                //Overrride location
+                double x = e.posX - min.getX();
+                double y = e.posY - min.getY();
+                double z = e.posZ - min.getZ();
+                NBTTagList l = new NBTTagList("Pos");
+                l.appendTag(new NBTTagDouble(null, x));
+                l.appendTag(new NBTTagDouble(null, y));
+                l.appendTag(new NBTTagDouble(null, z));
+                
+                
+                tag.setTag("Pos", l);
+                file.getEntities().add(tag);
+            }
+        }
 
         for(int y = 0;y<file.getHeight();y++){
             for(int x = 0;x<file.getWidth();x++){
@@ -40,7 +99,7 @@ public class Blueprint {
                     file.setBlockId(x, y,z,world.getBlockId(wx, wy, wz));
                     file.setBlockData(x, y,z,(byte) (world.getBlockMetadata(wx, wy, wz) & 0xFF));
                     
-                    if(!noTileEntityData){
+                    if(!ignoreTileEntityData){
                         TileEntity te = world.getBlockTileEntity(wx, wy, wz);
                         if(te!=null){
                             NBTTagCompound c = new NBTTagCompound();
@@ -57,8 +116,5 @@ public class Blueprint {
 
         return file;
     }
-
-
-
 
 }
