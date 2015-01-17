f8725b4a5d9c564726ef3fe027369d20460b424d
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2015-01-17 16:42:37 +0000

    Commented through.

--- src/main/java/com/tehbeard/forge/schematic/factory/worker/blocks/BlockRecord.java
+++ src/main/java/com/tehbeard/forge/schematic/factory/worker/blocks/BlockRecord.java
@@ -6,31 +6,74 @@ import net.minecraft.block.Block;
 /**
  * Class Name - BlockRecord
  * Package - com.tehbeard.forge.schematic.factory.worker.blocks
- * Desc of Class - ...
+ * Desc of Class - This class represents a single block and all the data
+ *                 associated with it (i.e. colour, rotation, etc.)
  * Author(s) - M. D. Ball
  */
 public final class BlockRecord {
+
+    /**
+     * The namespace is the designation of the block (i.e. "minecraft:stone")
+     */
     public final String namespace;
+
+    /**
+     * The id is the id of the object within the game registry and should probably
+     * not exist in here in favour of a method elsewhere which just gets the id of
+     * a given namespace
+     */
     public final int _id;
+
+    /**
+     * The metadata is all the surrounding data about a block, including its colour,
+     * rotation, etc.
+     */
     public final byte metadata;
 
+    /**
+     * Construct the record with the namespace of a block and the specified
+     * metadata that goes along with it
+     *
+     * @param namespace The designation of the block we're recording
+     * @param metadata The metadata associated with the block we're recording
+     */
     public BlockRecord(String namespace, byte metadata) {
         this.namespace = namespace;
         this._id = Block.getIdFromBlock(Block.getBlockFromName(namespace));
         this.metadata = metadata;
     }
 
+    /**
+     * Construct the record with the id of a block within the registry and the
+     * specified metadata that goes along with it
+     *
+     * @param id The id of the block we're recording (as found in the registry)
+     * @param metadata The metadata associated with the block we're recording
+     */
     public BlockRecord(int id, byte metadata) {
         this.namespace = GameData.getBlockRegistry().getNameForObject(Block.getBlockById(id));
         this._id = id;
-        this.metadata = (byte) 0;
+        this.metadata = metadata;
     }
 
+    /**
+     * Construct the record with the namespace of a block, assuming that there
+     * is no metadata to go along with it
+     *
+     * @param namespace The metadata associated with the block we're recording
+     */
     public BlockRecord(String namespace) {
         this(namespace, (byte) 0);
     }
 
+    /**
+     * Construct the record with the id of a block within the registry, assuming
+     * that there is no metadata to go along with it
+     *
+     * @param id The id of the block we're recording (as found in the registry)
+     */
     public BlockRecord(int id) {
         this(id, (byte) 0);
     }
+
 }
--- src/main/java/com/tehbeard/forge/schematic/factory/worker/blocks/ReplaceBlockWorker.java
+++ src/main/java/com/tehbeard/forge/schematic/factory/worker/blocks/ReplaceBlockWorker.java
@@ -8,41 +8,77 @@ import java.util.Map;
 /**
  * Class Name - ReplaceBlockWorker
  * Package - com.tehbeard.forge.schematic.factory.worker.blocks
- * Desc of Class - ...
+ * Desc of Class - A worker which, when given a map of blocks that
+ *                 it needs to convert against blocks that they're
+ *                 being converted to... Converts them.
  * Author(s) - M. D. Ball
  */
 public class ReplaceBlockWorker extends AbstractSchematicWorker {
 
-    Map<BlockRecord, BlockRecord> blockReplacements;
+    /**
+     * This is a map of records against records which is going to be used to
+     * say which blocks (with associated metadata) are going to be converted
+     * into which blocks (with more associated metadata)
+     */
+    private Map<BlockRecord, BlockRecord> blockReplacements;
 
+    /**
+     * Instantiate the class with the map of the blocks we're replacing
+     *
+     * @param blockReplacements A collection of records with one-to-one mapping
+     *                          of which blocks are going to be converted into
+     *                          which blocks
+     */
     public ReplaceBlockWorker(Map<BlockRecord, BlockRecord> blockReplacements) {
         this.blockReplacements = blockReplacements;
     }
 
+    /**
+     * As per the interface, when this is called, go through the schematic and
+     * replace blocks found in the blockReplacements map with their counterparts
+     * (also found in the blockReplacements map)
+     *
+     * @param original This is the schematic that we're going to iterate through
+     *                 and replace all the blocks that are present in the
+     *                 blockReplacements map
+     * @return The altered schematic with switched blocks
+     */
     @Override
     public SchematicFile modifySchematic(SchematicFile original) {
+
+        //For each block in the schematic...
         for (int y = 0; y < original.getHeight(); y++) {
             for (int x = 0; x < original.getWidth(); x++) {
                 for (int z = 0; z < original.getLength(); z++) {
+
+                    //If the block is found in the replacements map
                     if (blockReplacements.containsKey(
                             new BlockRecord(
                                     original.getBlockId(x, y, z),
                                     original.getBlockData(x, y, z)
                             ))) {
+
+                        //Then find out the replacement block
                         BlockRecord rec = blockReplacements.get(
                                 new BlockRecord(
                                         original.getBlockId(x, y, z),
                                         original.getBlockData(x, y, z)
                                 )
                         );
+
+                        //And replace the block with its counterpart
                         original.setBlockId(x, y, z, -1);
                         original.setBlockId(x, y, z, rec._id);
                         original.setBlockData(x, y, z, rec.metadata);
+
                     }
+
                 }
             }
         }
+
         return original;
+
     }
 
 }
